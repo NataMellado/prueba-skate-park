00eb3d79ffdd82f91170ed9b5ff34b7f6a19de03
Natalia Mellado <nataliamelladocani@gmail.com>
2024-04-26 18:23:10 -0400

    Added dependencies and server config

---
+++ db/db.js
@@ -0,0 +1,13 @@
+import pg from 'pg';
+const { Pool } = pg;
+import { config } from 'dotenv';
+
+config();
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false} // Para evitar errores de SSL en local, debes comentar esta línea cuando hagas el deploy en Render
+});
+
+export default pool;
+
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import routes from './src/routes/routes.js';
+import { engine } from "express-handlebars";
+import path from "path";
+import fileUpload from 'express-fileupload'
 import cookieParser from 'cookie-parser';
+process.loadEnvFile(); 
+const __dirname = path.resolve();
 
 // Server
 const app = express();
@@ -8,24 +13,31 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => { console.log(`Server is running on port http://localhost:${PORT}`); });
 
 // Configuración de middlewares
-app.use(express.static('public')); 
 app.use(express.json()); 
 app.use(cookieParser());
-
-// Configuración de motor de plantillas
-const exphbs = require("express-handlebars");
-app.set("view engine", "hbs");
-app.set("views", __dirname + "/src/views");
-app.engine(
-  "hbs",
-  exphbs.engine({
-    defaultLayaout: "main",
-    layoutsDir: __dirname + "/src/views/layouts",
-    partialsDir: __dirname + "/src/views/partials",
-    extname: "hbs",
+app.use(express.static('public')); 
+app.use(express.static(path.join(__dirname, "/views")));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
+app.use(express.urlencoded({ extended: false })); // Para que pueda recibir datos de formularios en req.body 
+app.use(
+  fileUpload({
+    limits: 5000000,
+    abortOnLimit: true,
+    responseOnLimit: "El tamaño de la imagen supera el límite permitido",
   })
 );
 
+
+// Configuración de handlebars
+app.set("view engine", "handlebars");
+app.engine(
+  "handlebars",
+  engine({
+    defaultLayout: "main",
+    layoutsDir: path.join(__dirname, "views/layouts"),
+  })
+);
 
 // Rutas de la API REST
 app.use('/api', routes);
