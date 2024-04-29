8a3903fabe2cdba310179d164d741a5ac28f752b
Natalia Mellado <nataliamelladocani@gmail.com>
2024-04-28 20:02:08 -0400

    Se comenta ssl en config.js

--- db/config.js
+++ db/config.js
@@ -6,7 +6,7 @@ config();
 
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false} // Para evitar errores de SSL en local, debes comentar esta línea cuando hagas el deploy en Render
+  // ssl: { rejectUnauthorized: false} // Para evitar errores de SSL en local, debes comentar esta línea cuando hagas el deploy en Render
 });
 
 export default pool;
