5eea5e167f82598d5e7cfcb3fc430e092bc3cea7
Natalia Mellado <nataliamelladocani@gmail.com>
2024-04-28 21:01:17 -0400

    Refactor fotoPath

--- api/controllers/authentication.controller.js
+++ api/controllers/authentication.controller.js
@@ -51,7 +51,7 @@ async function register (req, res) {
 
         // Foto
         const foto = req.files.foto;
-        const fotoPath = path.join('public', 'upload', foto.name);
+        const fotoPath = path.join('upload', `${Date.now()}_${foto.name}`);
         await foto.mv(fotoPath);
 
         // Crear un nuevo usuario
